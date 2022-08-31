b3cf5cc9472c4383c5d8425bc615d8be4a81f7a0
Tamer <47778933+tamerardal@users.noreply.github.com>
2022-08-31 14:12:10 +0300

    flatlist oluşturuldu.

--- src/components/ToDoCard/ToDoCardFlatList.js
+++ src/components/ToDoCard/ToDoCardFlatList.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import {FlatList} from 'react-native';
+import React, {useState} from 'react';
+import {FlatList, SafeAreaView, View, Text} from 'react-native';
+import ToDoCard from './ToDoCard';
+import tododata from '../../todo-data.json';
+import styles from './ToDoCard.styles';
 
 const ToDoCardFlatList = todo => {
+  const [todos, setTodos] = useState(tododata);
+  const renderToDo = ({item}) => <ToDoCard todo={item} />;
+  const renderKey = item => item.id.toString();
 
+  const activeTodo = todos.filter(todo => todo.isCompleted);
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={tododata}
+        renderItem={renderToDo}
+        keyExtractor={renderKey}
+        ListHeaderComponent={() => {
+          return (
+            <View style={styles.headerContainer}>
+              <Text style={styles.header}>Yapılacaklar</Text>
+              <Text style={styles.header}>{activeTodo.length}</Text>
+            </View>
+          );
+        }}
+      />
+    </SafeAreaView>
+  );
 };
 
 export default ToDoCardFlatList;
